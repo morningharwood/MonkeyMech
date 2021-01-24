65eeeb4481a3801f63b84780c9a82d44f4db98a7
Matthew Harwood <matthhar12@gmail.com>
2021-01-23 23:55:43 -0800

    Added Climbing
    Added Left and Right Velocities
    Added Climbing Scene Called BaseA

---
+++ Assets/Scenes/Storybook/Climbing Horizontal/Scripts/ClimbInteractor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClimbInteractor : XRBaseInteractable
+{
+    protected override  void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        base.OnSelectEntered(interactor);
+        if(interactor is XRDirectInteractor)
+        {
+            ActiveHand.current = interactor.name;
+        }
+            
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+        if(interactor is XRDirectInteractor)
+        {
+            if(ActiveHand.current == interactor.name)
+            {
+                ActiveHand.current = null;
+            }
+        }
+    }
+}
